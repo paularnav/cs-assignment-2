d8a5e56c0406d94036cfbf83ef38b762dc3a139d
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:27:02 +0530

    Create a2q6.cpp

---
+++ a2q6.cpp
@@ -0,0 +1,14 @@
+# cs-assignment-2
+//question 6
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+double a;
+cout<<"enter number: ";
+cin>>a;
+cout<<"square root of number is : "<<pow(a,0.5);
+return 0;
+}
