e9f62d8740f47c04c252b60eb51af6244c916891
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:21:48 +0530

    Create a2q3.cpp

---
+++ a2q3.cpp
@@ -0,0 +1,13 @@
+# cs-assignment-2
+//question 3
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b;
+cout<<"give temperature in fahrenheit";
+cin>>a;
+b=(a-32)*5/9;
+cout<< "temp in celsius ="<<b;
+return 0;
+}
