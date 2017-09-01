3c251d5c0a98b7ea8ddbc1f32e8b18a2c3cfefab
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:29:27 +0530

    Create a2q8.cpp

---
+++ a2q8.cpp
@@ -0,0 +1,17 @@
+# cs-assignment-2
+//question 8
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+double a,b,c;
+cout<<"enter base length of triangle : ";
+cin>>a;
+cout<<"enter height length of triangle : ";
+cin>>b;
+c=a*b*0.5;
+cout<<"area of triangle : "<<c;
+return 0;
+}
