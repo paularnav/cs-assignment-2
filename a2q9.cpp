d67f065284e1ec14ef055c99ef9f297ea6a09aa6
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:30:33 +0530

    Create a2q9.cpp

---
+++ a2q9.cpp
@@ -0,0 +1,15 @@
+# cs-assignment-2
+//question 9
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+double a,b;
+cout<<"enter side length of triangle : ";
+cin>>a;
+b=1.73*a*a/4;
+cout<<"area of equilateral triangle : "<<b;
+return 0;
+}
