5b71bd331e6047bcbc6e77ac5231102526c0ac01
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:28:14 +0530

    Create a2q7.cpp

---
+++ a2q7.cpp
@@ -0,0 +1,17 @@
+# cs-assignment-2
+//question 7
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+double a,b,c;
+cout<<"enter first angle of triangle : ";
+cin>>a;
+cout<<"enter second angle of triangle : ";
+cin>>b;
+c= 180-a-b;
+cout<<"third angle of triangle is : "<<c;
+return 0;
+}
