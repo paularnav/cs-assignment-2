78d085800943a08cf2c495ef71e1cc37074a89a8
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:25:50 +0530

    Create a2q5.cpp

---
+++ a2q5.cpp
@@ -0,0 +1,16 @@
+# cs-assignment-2
+//question 5 
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+double a,b;
+cout<<"enter base: ";
+cin>>a;
+cout<<"enter power: ";
+cin>>b;
+cout<<"power of number is : "<<pow(a,b);
+return 0;
+}
