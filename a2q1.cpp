6210e4a1f9be39071286eabe6a55bb0a897ac50c
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:18:45 +0530

    Create a2q1.cpp

---
+++ a2q1.cpp
@@ -0,0 +1,15 @@
+# cs-assignment-2
+//question 1
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b,c;
+cout << "enter length in cm";
+cin >> a;
+b=a/100;
+c=a/100000;
+cout<< "length in meter  ="<<b;
+cout << "length in kilometer ="<<c;
+return o;
+}
